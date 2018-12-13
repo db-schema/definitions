194173d9d305eb44c122cfe9f6ad576dbf5dceb4
Vsevolod Romashov <7@7vn.ru>
2018-12-14 02:36:18 +0300

    add serial pseudo types

---
+++ lib/db_schema/definitions/field/serial.rb
@@ -0,0 +1,17 @@
+module DbSchema
+  module Definitions
+    module Field
+      class SmallSerial
+        register :smallserial
+      end
+
+      class Serial
+        register :serial
+      end
+
+      class BigSerial
+        register :bigserial
+      end
+    end
+  end
+end
